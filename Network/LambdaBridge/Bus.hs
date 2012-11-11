b76145eb7d41c960957a0b3806273c89c42da80e
Andy Gill <andygill@ku.edu>
2012-11-10 20:29:33 -0600

    Refactoring the bus API. Can use a monad!

--- Network/LambdaBridge/Bus.hs
+++ Network/LambdaBridge/Bus.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE KindSignatures, GADTs, FlexibleInstances, ScopedTypeVariables #-}
+{-# LANGUAGE KindSignatures, GADTs, FlexibleInstances, RankNTypes, ScopedTypeVariables #-}
 module Network.LambdaBridge.Bus where
 
 import Network.LambdaBridge.Bridge
@@ -6,14 +6,19 @@ import Network.LambdaBridge.Bridge
 
 import Control.Concurrent
 import Control.Concurrent.MVar
+import Control.Concurrent.Chan
 import Control.Applicative
 import Data.Monoid
 import Data.Word
 import Control.Monad.Writer
-import Data.Map as Map
+import Control.Monad.State
+import Data.Map as Map hiding (singleton)
 
 import qualified Data.ByteString as BS
 import Data.ByteString (ByteString)
+
+import Control.Monad.Operational
+
 
 {-
 data BusCmdPacket = BusCmdPacket U16 [BusCmd]
@@ -26,18 +31,41 @@ data BusCmd
 data BusReplyPacket = BusReplyPacket U16 [U8]
 -}
 
+data Remote a
+        = Symbol
+        | Remote a
+
+instance Functor Remote where
+        fmap f Symbol = Symbol
+        fmap f (Remote a) = Remote (f a)
+
+instance Applicative Remote where
+        pure a = Remote a
+        Symbol <*> _ = Symbol
+        _ <*> Symbol = Symbol
+        (Remote f) <*> (Remote a) = Remote (f a)
 
 data BusCmd :: * -> * where
         BusWrite :: Word16 -> Word8     -> BusCmd ()
-        BusRead  :: Word16              -> BusCmd Word8
+        BusRead  :: Word16              -> BusCmd (Remote Word8)
 
         Pure     :: a                   -> BusCmd a
-        Apply    :: BusCmd (a -> b)
-                 -> BusCmd a            -> BusCmd b
+        Bind     :: BusCmd a
+                 -> (a -> BusCmd b)     -> BusCmd b
+
+
+type BusM a = Program BusCmd a
 
 instance Applicative BusCmd where
         pure = Pure
-        (<*>) = Apply
+        b1 <*> b2 = do
+                f <- b1
+                a <- b2
+                return (f a)
+
+instance Monad BusCmd where
+        return = Pure
+        (>>=) = Bind
 
 instance Monoid (BusCmd ()) where
         mempty = pure ()
@@ -46,40 +74,82 @@ instance Monoid (BusCmd ()) where
 instance Functor BusCmd where
         fmap f a = pure f <*> a
 
+done :: BusM (Remote ())
+done = return (pure ())
+
 newtype Board = Board (Frame -> IO (Maybe Frame))       -- Will time out in a Board-specific way
 --                      ((Frame -> IO ()) -> IO ())       -- request callback on interupt
 
 -- send, waits for response, can time out
-send :: Board -> BusCmd a -> IO (Maybe a)
+send :: Board -> BusM (Remote a) -> IO (Maybe a)
 send (Board fn) cmds = do
-        let req_msg = cmdToRequest cmds
+        let (_,req_msg) = runWriter (cmdToRequest cmds)
         print req_msg
         rep_msg <- fn (Frame (BS.pack req_msg))
         print rep_msg
         case rep_msg of
           Nothing -> return Nothing
           Just (Frame rep) ->
-                    case cmdWithReply cmds (BS.unpack rep) of
-                        Just (a,[]) -> return (Just a)
-                        Just (a,_)  -> return Nothing -- fail "bad format replied"
+                    case runStateT (cmdWithReply cmds) (BS.unpack rep) of
+                        Just (Remote a,[]) -> return (Just a)
+                        Just (a,_)  -> return Nothing -- fail "bad format, bad remote, etc"
                         Nothing     -> return Nothing
 
 
-cmdToRequest :: BusCmd a -> [Word8]
-cmdToRequest (BusWrite addr val) = [tagWrite] ++ seq16 addr ++ [val]
-cmdToRequest (BusRead addr)      = [tagRead] ++ seq16 addr
-cmdToRequest (Pure a)            = []
-cmdToRequest (Apply b1 b2)       = cmdToRequest b1 ++ cmdToRequest b2
+walk :: Monad m => (forall a . BusCmd a -> m a) -> BusCmd a -> m a
+walk _ (Pure a) = return a
+walk fn (Bind m k) = do
+        a <- walk fn m
+        walk fn (k a)
+walk fn other = fn other
 
-cmdWithReply :: BusCmd a -> [Word8] -> Maybe (a,[Word8])
-cmdWithReply (BusWrite {})      rep  = return ((),rep)
-cmdWithReply (BusRead _)   (val:rep) = return (val,rep)
-cmdWithReply (BusRead _)          [] = fail "bus reply error"
-cmdWithReply (Pure a)           rep  = return (a,rep)
-cmdWithReply (Apply b1 b2)      rep0 = do
-        (f,rep1) <- cmdWithReply b1 rep0
-        (a,rep2) <- cmdWithReply b2 rep1
-        return (f a,rep2)
+cmdToRequest :: BusM a -> Writer [Word8] a
+cmdToRequest = interp $ \ cmd -> case cmd of
+        BusWrite addr val -> do
+                tell $ [tagWrite] ++ seq16 addr ++ [val]
+                return ()
+        BusRead addr -> do
+                tell $ [tagRead] ++ seq16 addr
+                return Symbol
+
+interp :: forall a c m . (Monad m) => (forall a . c a -> m a) -> Program c a -> m a
+interp f = eval . view
+  where eval :: forall a . ProgramView c a -> m a
+        eval (Return a) = return a
+        eval (m :>>= i) = do
+                r <- f m
+                interp f (i r)
+
+{-
+reifyBusCmd :: [Word8] -> BusCmd (Remote [Word8])
+reifyBusCmd (tag:h1:l1:val:rest) | tag == tagWrite =
+        BusWrite addr val *> reifyBusCmd rest
+  where
+          addr = unseq16 [h1,l1]
+reifyBusCmd (tag:h1:l1:rest) | tag == tagRead =
+        liftA2 (:) (BusRead addr) (reifyBusCmd rest)
+  where
+          addr = unseq16 [h1,l1]
+reifyBusCmd [] = pure []
+
+prop_reqReify cmd = xs == cmdToRequest (reifyBusCmd xs)
+  where xs = cmdToRequest cmd
+-}
+
+cmdWithReply :: BusM a -> StateT [Word8] Maybe a
+cmdWithReply = interp $ \ cmd -> case cmd of
+        BusWrite addr val -> return ()
+        BusRead addr -> do
+                val <- item
+                return (Remote val)
+
+item :: StateT [Word8] Maybe Word8
+item = do
+        chs <- get
+        case chs of
+          [] -> fail "item failed"
+          (c:cs) -> do put cs
+                       return c
 
 tagWrite, tagRead :: Word8
 tagWrite = 0x1
@@ -95,13 +165,16 @@ seq16 v = [hi v,low v]
 unseq16 :: [Word8] -> Word16
 unseq16 [h,l] = fromIntegral h * 256 + fromIntegral l
 
+busWrite addr val = singleton $ BusWrite addr val
+busRead addr = singleton $ BusRead addr
 
 test = do
-        send brd $
-                BusWrite 0 1    *>
-                BusWrite 2 3    *>
-                BusRead 5       <*
-                BusWrite 9 10
+        send brd $ do
+                busWrite 0 1
+                busWrite 2 3
+                busRead 5
+                busWrite 9 10
+                done
 
 brd = Board $ error ""
 --boardFromBridgeFrame :: Bridge Frame -> IO Board
@@ -152,6 +225,8 @@ connectBoard timeoutTime bridge = do
 
         return $ Board send
 
+-----------------------------------------------------------------------------
+
 -- General code
 data Callback k a = Callback (MVar (Map k (a -> IO ())))
 
@@ -168,3 +243,36 @@ callback (Callback callbacks) key val = do
           Just f -> do
                 putMVar callbacks (delete key fm)
                 f val
+
+-----------------------------------------------------------------------------
+
+data BusFrame = BusFrame Word16 [Word8]
+        deriving (Show)
+
+readBusFrame :: Frame -> Maybe BusFrame
+readBusFrame (Frame bs0) =
+        case do (a,bs1) <- BS.uncons bs0
+                (b,bs2) <- BS.uncons bs1
+                return (unseq16 [a,b],bs2) of
+          Just (uq,rest) -> return (BusFrame uq (BS.unpack rest))
+          Nothing -> fail "bad packet format"
+
+showBusFrame :: BusFrame -> Frame
+showBusFrame (BusFrame uq msg) = Frame (BS.append (BS.pack (seq16 uq)) (BS.pack msg))
+
+
+
+interpBus :: (BusCmd [Word8] -> IO (Maybe [Word8])) -> IO (Bridge Frame)
+interpBus cmd = do
+        cmdChan <- newChan
+        resChan <- newChan
+
+        --
+        forkIO $ forever $ do
+                frame <- readChan cmdChan
+
+                return ()
+
+        return $ Bridge { toBridge = writeChan cmdChan
+                        , fromBridge = readChan resChan
+                        }
