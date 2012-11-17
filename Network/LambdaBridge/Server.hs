cf3c057847e19f9c9caa9ec10d6106b635d23a3a
Andy Gill <andygill@ku.edu>
2012-11-16 22:46:42 -0600

    Fixing server.

--- Network/LambdaBridge/Server.hs
+++ Network/LambdaBridge/Server.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, GADTs, DataKinds, KindSignatures #-}
+
 -- | This module provide the ability to build a server.
 
 -- This can be used for building simulators of hardware board,
@@ -8,6 +10,7 @@ module Network.LambdaBridge.Server (serverBridge) where
 
 import Network.LambdaBridge.Bridge
 import Network.LambdaBridge.Logging
+import Network.LambdaBridge.Handle
 
 import System.IO
 import Data.Char
@@ -21,6 +24,8 @@ import Data.Binary.Get
 import qualified Data.ByteString as BS
 import Control.Concurrent
 
+
+
 import Network
 --import Network.Socket hiding (send, sendTo, recv, recvFrom)
 --import Network.Socket.ByteString
@@ -33,27 +38,11 @@ debug = debugM "lambda-bridge.server"
 -- In typical useage, Lambda Bridge does not use (software) servers
 -- except for testing, or perhaps forwarding to real hardware.
 --
-serverBridge :: PortID -> IO (Bridge framing integrity)
-serverBridge portId = error ""
-{-
-        finally (do sock <- listenOn portId
-                    forever $ do
-                       debug $ "accepting socket " ++ show sock
-                       acc@(hd,_host,_port) <- accept sock
-                       debug $ "accepted " ++ show acc
-                       server hd)
-                (case portId of
-                   UnixSocket name -> removeFile name
-                   _ -> return ())
-  where
-    server hd = do
-        hSetBuffering hd NoBuffering            -- for now
-        forkIO $ forever $ do
-                bs <- BS.hGet hd 1
-                if BS.length bs == 0 then fail "socket closed"
-                                     else toBridge bridge bs
+serverBridge :: PortID -> IO (Bridge Streamed Trustworthy)
+serverBridge portId =
+        do sock <- listenOn portId
+           debug $ "accepting socket " ++ show sock
+           acc@(hd,_host,_port) <- accept sock
+           debug $ "accepted " ++ show acc
+           handleBridge hd hd
 
-        forever $ do
-                bs <- fromBridge bridge
-                BS.hPut hd bs
--}
