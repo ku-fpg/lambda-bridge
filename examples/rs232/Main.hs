d7c7b3872c4f8b87154f16be505cfc2f675c7325
Andy Gill <andygill@ku.edu>
2012-11-18 11:40:48 -0600

    Simulator testing.

--- examples/rs232/Main.hs
+++ examples/rs232/Main.hs
@@ -1,17 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Network.LambdaBridge.Bridge
 import Network.LambdaBridge.SLIP
 import Network.LambdaBridge.CRC
-import Network.LambdaBridge.Bus
-import Network.LambdaBridge.Bus.Simulator
+import Network.LambdaBridge.Bus hiding (debug)
+import Network.LambdaBridge.Logging
+import Network.LambdaBridge.Bus.Simulator hiding (debug)
 
 import qualified Data.ByteString as BS
 import Control.Concurrent
 import Control.Monad
 import Data.Monoid
 import Data.String
+import Data.Char
 import System.IO
+import Data.Time.Clock
+import System.Random
+
+debug = debugM "lambda-bridge.main"
+
 
 io b = do
         forkIO $ forever $ do
@@ -27,10 +35,22 @@ io b = do
 
 simBoard :: IO Board
 simBoard = do
+
         v1 <- newEmptyMVar
+        v2 <- newEmptyMVar
 
         brd0 <- stateMachineToBus rxInitialState $ rxStateMachine v1
-        brd2 <- stateMachineToBus txInitialState $ txStateMachine v1
+        brd2 <- stateMachineToBus txInitialState $ txStateMachine v2
+
+        forkIO $ forever $ do
+                v <- takeMVar v1
+                debug $ "got " ++ show v
+                putMVar v2 (v)
+                debug $ "sent " ++ show v
+
+--        forkIO $ forever  $ do
+--                sequence [ putMVar v2 n | n <- [100..200]]
+
 
         let brd = virtualBus
                 [ (0, brd0)
@@ -40,7 +60,44 @@ simBoard = do
         return brd
 
 
+
+test_baud rate = do
+        -- our backbone, level 0
+        (lhs0,rhs0) <- connection
+
+        lhs1 <- baud rate  (serialize lhs0)
+        rhs1 <- baud rate  (serialize rhs0)
+
+        echo id rhs1    -- cap the far end
+
+        tm0 <- getCurrentTime
+
+        count <- newMVar 0
+
+        forkIO $ forever $ do
+                toBridge lhs1 $ BS.pack [33..100]
+
+        let pp n | isAscii n || n == '\n' = [n]
+                 | otherwise = "."
+
+        forever $ do
+                bs <- fromBridge lhs1
+                n <- takeMVar count
+                let n' = n + BS.length bs
+                putMVar count n'
+                tm1 <- getCurrentTime
+                let diff :: Float = realToFrac $ tm1 `diffUTCTime` tm0
+                if (n `mod` 1000 == 0) then
+                        putStrLn $ show n' ++ " bytes in " ++ show diff ++ " seconds, rate = "
+                                ++ show (floor (1 / (diff / fromIntegral n')) )
+                    else return ()
+
+main2 "115200" = test_baud 115200
+main2 "9600"   = test_baud 9600
+
 main = do
+        setStdGen (mkStdGen 1000)
+        init_logging
         hSetBuffering stdout LineBuffering
         hSetBuffering stdout LineBuffering
 
@@ -49,43 +106,52 @@ main = do
         -- our backbone, level 0
         (lhs0,rhs0) <- connection
 
-{-
+        -- introduce delays
+        let rate = 9600
+        lhs1 <- baud rate  (serialize lhs0)
+        rhs1 <- baud rate  (serialize rhs0)
+
+        -- introduce errors
+
+        lhs2 <- noise 0.01 (serialize lhs1)
+                                        ; rhs2 <- noise 0.01 (serialize rhs1)
+
+
         -- level 1                              simulator stack
-        lhs1 <- slipProtocol lhs0       ;   rhs1 <- slipProtocol rhs0
+        lhs3 <- slipProtocol lhs2       ;   rhs3 <- slipProtocol rhs2
 
         -- level 2
 
-        lhs2 <- crc16Protocol lhs1      ;   rhs2 <- crc16Protocol rhs1
+        lhs4 <- crc16Protocol lhs3      ;   rhs4 <- crc16Protocol rhs3
 
         -- level 3
--}
 
-{-
-        lhs_brd <- connectToBoard 1.0 (checked lhs0)
-                                        ; interpBus (checked rhs0) sim_brd
+        lhs_brd <- connectToBoard 0.1 lhs4
+                                        ; interpBus rhs4 sim_brd
 
--}
-        p0 <- busWritePort sim_brd 0
-        p2 <- busReadPort sim_brd 2
+        -- with the bridge set up, we can do testing
+
+
+        p0 <- busWritePort lhs_brd 0
+        p2 <- busReadPort lhs_brd 2
+
+        let text :: [String]
+            text = [ show n ++ " + " ++ show n ++ " = " ++ show (n+n) ++ "\n"
+                   | n <- [0..100]
+                   ]
 
         forkIO $ do
-                sequence_ [ writePort p0 (BS.pack [n])
-                          | n <- take 10000 $ cycle [0..255]
+                sequence_ [ writePort p0 $ BS.pack $ map (fromIntegral . ord) txt
+                          | txt <- text
                           ]
+
+        let pp n | isAscii n = [n] -- ['<',n,'>']
+                 | otherwise = "."
 
         forever $ do
                 bs <- readPort p2
-                print bs
+                putStr $ concatMap pp $ map (chr . fromIntegral) $ BS.unpack $ bs
+                hFlush stdout
 
 
-{-
-        echo id b2 -- (\ bs -> fromString ("<" ++ show bs ++ ">")) b2
-
-        b3 <- noise 0.01 b1
-        b4 <- buffer 1 b3
-        b5 <- slipProtocol b4
-        b6 <- crc16Protocol b5
-
-        io b6
--}
         return ()
